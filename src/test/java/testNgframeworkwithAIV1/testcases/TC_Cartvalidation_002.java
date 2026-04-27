980d78591a3b63922d741740598fe53dbeca825c
suganthiven-png <suganthi.ven@googlemail.com>
2026-04-27 17:51:16 +0200

    Refactor cart validation test case

--- src/test/java/testNgframeworkwithAIV1/testcases/TC_Cartvalidation_002.java
+++ src/test/java/testNgframeworkwithAIV1/testcases/TC_Cartvalidation_002.java
@@ -1,9 +1,5 @@
 package testNgframeworkwithAIV1.testcases;
 
-import static org.testng.Assert.assertEquals;
-
-import java.util.Map;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,23 +8,25 @@ import testNgframeworkwithAIV1.pageobjects.CartPage;
 import testNgframeworkwithAIV1.pageobjects.InventoryPage;
 import testNgframeworkwithAIV1.pageobjects.Loginpage;
 
-public class TC_Cartvalidation_002 extends BaseTest{
-	@Test
-	public void cartvalidationchech() {
-		// Perform login for this test explicitly
-		Loginpage login = new Loginpage(driver);
-		login.enterUsername("standard_user");
-		login.enterPassword("secret_sauce");
-		login.clickLogin();
-	    InventoryPage inventory=new InventoryPage(driver);
-		Product product=inventory.getfirstproductname();
-		// Use no-wait variant to avoid flaky cart badge timing
-		inventory.addFirstProductToCartNoWait();
-		inventory.cartclick();
-		CartPage cart=new CartPage(driver);
-		Product productcart=cart.getcardInformation();
-		Assert.assertEquals(productcart.getName(),product.getName());
-		Assert.assertEquals(productcart.getPrice(),product.getPrice());
-	}
+public class TC_Cartvalidation_002 extends BaseTest {
 
+    @Test
+    public void cartvalidationchech() {
+        Loginpage login = new Loginpage(driver);
+        login.enterUsername("standard_user");
+        login.enterPassword("secret_sauce");
+        login.clickLogin();
+
+        InventoryPage inventory = new InventoryPage(driver);
+        Product product = inventory.getfirstproductname();
+
+        inventory.addFirstProductToCartNoWait();
+        inventory.cartclick();
+
+        CartPage cart = new CartPage(driver);
+        Product productcart = cart.getFirstCartProduct();
+
+        Assert.assertEquals(productcart.getName(), product.getName());
+        Assert.assertEquals(productcart.getPrice(), product.getPrice());
+    }
 }
